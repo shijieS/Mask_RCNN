9f5bc72cf0d98e6cd350804764b23118b3b30c40
shijieSun <1029783071@qq.com>
2017-12-05 09:32:59 +0800

    add ProcessingVideos.ipynb
    Signed-off-by: shijieSun <1029783071@qq.com>

---
+++ ProcessVideo.py
@@ -0,0 +1,76 @@
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+import coco
+import utils
+import model as modellib
+import visualize
+
+import cv2
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Path to trained weights file
+# Download this file and place in the root of your
+# project (See README file for details)
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "model\\mask_rcnn_coco.h5")
+COCO_VIDEO_PATH = os.path.join(ROOT_DIR, "videos")
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+
+class InferenceConfig(coco.CocoConfig):
+    GPU_COUNT = 1
+    IMAGE_PER_GPU = 1
+
+
+config = InferenceConfig()
+config.display()
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+video_names = next(os.walk(COCO_VIDEO_PATH))[2]
+
+for video_name in video_names:
+    vc = cv2.VideoCapture(video_name)
+    if not vc.isOpened():
+        print('cannot open', video_name)
+    print('handling ', video_name)
+    ret, frame = vc.read()
+    while ret:
+        # translate nparray to sci-image
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = model.detect([frame], verbose=0)
+
+        ret, frame = vc.read()
